b77224b6d6c5c7f9f8164af55319e6f151f0f65e
İnci Aliyeva <95902681+Incialiyeva@users.noreply.github.com>
2025-07-22 14:16:44 +0300

    Revert "Add global styles and background to the application, integrate CodeEditor component"
    This reverts commit 9390ba05da749e4500c85934152c9d175d2c9090.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CodeEditorComponent } from './code-editor/code-editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CodeEditorComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
